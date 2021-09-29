dbe6b341c92f8bb69083cf33640e5a598e02d928
NagarajuAlladi <nagaraju.alladi@bildemp.com>
2021-09-29 19:09:42 +0530

    made some changes

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,18 +1,43 @@
-import React from 'react'
-import styles from '../styles/Dashboard/Navbar.module.css'
+import { Button, Typography } from "@material-ui/core";
+import React from "react";
+import styles from "../styles/Dashboard/Navbar.module.css";
 
 function Navbar() {
-    return (
-        <div className={styles.outerDiv}>
-            <div className={styles.leftDiv}>
-                <h2>OpenSeats</h2>
-            </div>
-            <div className={styles.rightDiv}>
-                <button>Job Drafts</button>
-                <button className={styles.buttonTwo}>+New Department</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.outerDiv}>
+      <div className={styles.leftDiv}>
+        <h2>OpenSeats</h2>
+      </div>
+      <div className={styles.rightDiv}>
+        <Button
+          variant="contained"
+          variant="text"
+          size="small"
+          style={{
+            color: "rgb(58,112,209)",
+            padding: "10px 10px",
+            border: " 0.001rem solid rgb(194, 190, 190)",
+          }}
+        >
+          <Typography variant="caption">Job Drafts</Typography>
+        </Button>
+
+        <Button
+          variant="contained"
+          variant="text"
+          size="small"
+          style={{
+            marginLeft: "10px",
+            padding: "10px 10px",
+            color: "white",
+            backgroundColor: "rgb(58,112,209)",
+          }}
+        >
+          <Typography variant="caption"> +New Department</Typography>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
